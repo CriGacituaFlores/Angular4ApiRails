8b3e951d0dad331b1632ee1c0ea5f3c3cb1861a8
cristian <cri.gacitua.flores@gmail.com>
2017-08-30 00:18:51 -0300

    add observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
@@ -26,7 +27,9 @@ import {AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    DocumentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
