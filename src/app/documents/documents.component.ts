d040eda0348f07554a9dde58b19b3a4ea9c58c7a
cristian <cri.gacitua.flores@gmail.com>
2017-08-30 23:49:30 -0300

    add css

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -24,8 +24,8 @@ export class DocumentsComponent implements OnInit {
 
   ngOnInit() {
     let timer = Observable.timer(0, 5000);
-    timer.subscribe(() => this.getDocuments);
-    this.getDocuments();
+    timer.subscribe(() => this.getDocuments());
+    //this.getDocuments();
   }
 
   getDocuments(){
